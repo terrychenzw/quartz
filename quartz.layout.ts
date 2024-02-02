367b1a836ab8ce663dfc289ee827aa6155efa62a
terrychenzw <terrychenzw@gmail.com>
2024-02-02 22:29:00 +0800

    Quartz sync: Feb 2, 2024, 10:28 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,6 +5,11 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
+  footer: Component.Footer({
+    links: {
+      GitHub: "https://github.com/jackyzha0/quartz",
+      "Discord Community": "https://discord.gg/cRFFHYye7t",
+    },
   }),
 }
 
@@ -38,8 +43,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-  ],
-  right: [
     Component.DesktopOnly(Component.Explorer()),
-],
+  ],
+  right: [],
 }
