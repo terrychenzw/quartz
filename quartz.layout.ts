e3207ca7a4c6cbf6c9adcf418042ccef28cd1a38
terrychenzw <terrychenzw@gmail.com>
2024-02-02 19:25:03 +0800

    Quartz sync: Feb 2, 2024, 7:25 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -43,7 +43,8 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
   ],
-  right: [],
+  right: [
+    Component.DesktopOnly(Component.Explorer()),
+],
 }
